27855def60587a607fb2ab0c4b31a36a288f1570
Miho Funayama <funamiho1011@gmail.com>
2024-08-14 21:56:14 +0900

    moved 'checkout_sessions' under API folder, added flashcard content, and dialog when saving flashcards

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -7,13 +7,13 @@ You are a flashcard creator. Your task is to generate concise and effective flas
 1. Create clear and concise questions for the front of the flashcards.
 2. Provide accurate and informative answers for the back of the flashcards.
 3. Ensure that each flashcards focuses on a single concept or piece of information.
-4. Use simple language to make the flashcards accessible to a wide range of learners. 
+4. Use simple language to make the flashcards accessible to a wide range of learners.
 5. Include a variety of question types, such as definitions, examples, comparisons, and applications.
-6. Avoid overly complex or ambiguous or memory  aids to help reinforce the information. 
+6. Avoid overly complex or ambiguous or memory  aids to help reinforce the information.
 7. When appropriate, use mnemonics or memory aids to help reinforce the information.
-8. Tailor the difficulty level of the flashcards to the user's specified preferences. 
+8. Tailor the difficulty level of the flashcards to the user's specified preferences.
 9. If given a body of text, extract the most important and relevant information for the flashcards.
-10. Aim to create a balanced set of flashcards that covers the topic comprehensively. 
+10. Aim to create a balanced set of flashcards that covers the topic comprehensively.
 11. Only generate 10 flashcards.
 Remember, the goal is to facilitate effective learning and retention of information through these flashcards.
 
@@ -29,10 +29,10 @@ Return in the following JSON format
 `
 
 export async function POST(req) {
-    const openai = OpenAI()
+    const openai = new OpenAI()
     const data = await req.text()
 
-    const completion = await openai.chat.completion.create({
+    const completion = await openai.chat.completions.create({
         messages: [
             {role: 'system', content: systemPrompt},
             {role: 'user', content: data},
@@ -40,7 +40,7 @@ export async function POST(req) {
         model: "gpt-4o",
         response_format: {type: 'json_object'},
     })
-    const flashcards = JSON.parse(completion.choices[0].messages.content)
+    const flashcards = JSON.parse(completion.choices[0].message.content)
 
-    return NextResponse.json(flashcards.flashcard)
+    return NextResponse.json(flashcards.flashcards)
 }
