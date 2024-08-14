18c3273a1630373a873eb1ab136184f8a13d2a27
Miho Funayama <funamiho1011@gmail.com>
2024-08-14 15:00:43 +0900

    store api keys in .env file for security

--- firebase.js
+++ firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCXrC0CVj-km-WyQtzMQfSRDSiaYgHcqjo",
-  authDomain: "flashcards-91cd1.firebaseapp.com",
-  projectId: "flashcards-91cd1",
-  storageBucket: "flashcards-91cd1.appspot.com",
-  messagingSenderId: "519939159765",
-  appId: "1:519939159765:web:6d6346839cf6fd4601545c",
-  measurementId: "G-GJGZ1GT2FH"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
