210568790b0c35b1cd8952f7962580b42a1bc5d5
Miho Funayama <funamiho1011@gmail.com>
2024-08-15 19:30:06 +0900

    installed Acernity UI and implemented animation card and buttons

--- app/page.js
+++ app/page.js
@@ -1,50 +1,69 @@
-'use client'
-import Image from "next/image";
+"use client";
 import getStripe from "@/utils/get-stripe";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { AppBar, Box, Button, Container, Grid, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  Grid,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { CardBody, CardContainer, CardItem } from "../components/ui/3d-card";
+
 import Head from "next/head";
 
 // please check the container to be 100%
 export default function Home() {
   const handleSubmit = async () => {
-    const checkoutSession = await fetch('/api/checkout_session', {
-      method: 'POST',
+    const checkoutSession = await fetch("/api/checkout_session", {
+      method: "POST",
       headers: {
-        origin: 'http://localhost:3000'
+        origin: "http://localhost:3000",
       },
-    })
+    });
 
     console.log(checkoutSession);
-    const checkoutSessionJson = await checkoutSession.json()
+    const checkoutSessionJson = await checkoutSession.json();
 
     if (checkoutSession.statusCode === 500) {
       console.error(checkoutSession.message);
-      return
+      return;
     }
 
-    const stripe = await getStripe()
-    const {error} = await stripe.redirectToCheckout({
+    const stripe = await getStripe();
+    const { error } = await stripe.redirectToCheckout({
       sessionId: checkoutSessionJson.id,
-    })
+    });
 
     if (error) {
       console.warn(error.message);
     }
-  }
+  };
   return (
     <Container maxWidth="100vw">
       <Head>
         <title>Flashcard SaaS</title>
-        <meta name = "description" content="Create flashcard from your text"></meta>
+        <meta
+          name="description"
+          content="Create flashcard from your text"
+        ></meta>
+        cod
       </Head>
 
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" style={{flexGrow: 1}}>Flashcard SaaS</Typography>
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
+            Flashcard SaaS
+          </Typography>
           <SignedOut>
-            <Button color="inherit" href="sign-in">LogIn</Button>
-            <Button color="inherit" href="sign-up">SignUp</Button>
+            <Button color="inherit" href="sign-in">
+              LogIn
+            </Button>
+            <Button color="inherit" href="sign-up">
+              SignUp
+            </Button>
           </SignedOut>
           <SignedIn>
             <UserButton />
@@ -52,84 +71,135 @@ export default function Home() {
         </Toolbar>
       </AppBar>
 
-      <Box sx={{
-        textAlign: 'center',
-        my: 4
-
-      }}>
-        <Typography variant="h2" gutterBottom>Welcome to Flashcard SaaS</Typography>
+      <Box
+        sx={{
+          textAlign: "center",
+          my: 4,
+        }}
+      >
+        <Typography variant="h2" gutterBottom>
+          Welcome to Flashcard SaaS
+        </Typography>
         <Typography variant="h5" gutterBottom>
-          {' '}
+          {" "}
           The easiest way to make flashcards from text
         </Typography>
-        <Button variant="contained" color="primary" sx={{mt: 2}}>
-          Get Started
-        </Button>
+        <button className="p-[3px] relative" sx={{ mt: 2 }} onClick={() => window.location.href = "/generate"}>
+                    <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+                    <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
+                    Get Started
+                    </div>
+                  </button>
       </Box>
-      <Box sx = {{my: 6}}>
-        <Typography variant="h4" gutterBottom>Features</Typography>
+      <Box sx={{ my: 6 }}>
+        <Typography variant="h4" gutterBottom>
+          Features
+        </Typography>
         <Grid container spacing={4}>
           <Grid item xs={12} md={4}>
-            <Typography variant="h6" gutterBottom>Easy Text Input</Typography>
+            <Typography variant="h6" gutterBottom>
+              Easy Text Input
+            </Typography>
             <Typography>
-              { ' ' }
-              Simply input your text and let our software do the rest. Creating flashcards has never been easier.
+              {" "}
+              Simply input your text and let our software do the rest. Creating
+              flashcards has never been easier.
             </Typography>
           </Grid>
           <Grid item xs={12} md={4}>
-            <Typography variant="h6" gutterBottom>Smart Flashcards</Typography>
+            <Typography variant="h6" gutterBottom>
+              Smart Flashcards
+            </Typography>
             <Typography>
-              { ' ' }
-              Our AI intelligently breaks down your text into concise flashcards, perfect for studying.
+              {" "}
+              Our AI intelligently breaks down your text into concise
+              flashcards, perfect for studying.
             </Typography>
           </Grid>
           <Grid item xs={12} md={4}>
-            <Typography variant="h6" gutterBottom>Accessible Anywhere</Typography>
+            <Typography variant="h6" gutterBottom>
+              Accessible Anywhere
+            </Typography>
             <Typography>
-              { ' ' }
-              Access your flashcards from any device, at any time. Study on the go with ease.
+              {" "}
+              Access your flashcards from any device, at any time. Study on the
+              go with ease.
             </Typography>
           </Grid>
         </Grid>
       </Box>
-      <Box sx={{my: 6, textAlign: 'center'}}>
-        <Typography variant="h4" gutterBottom>Pricing</Typography>
+      <Box sx={{ my: 3, textAlign: "center" }}>
+        <Typography variant="h4" gutterBottom>
+          Pricing
+        </Typography>
         <Grid container spacing={4}>
           <Grid item xs={12} md={6}>
-            <Box sx={{
-                p: 3,
-                border: '1px solid',
-                borderColor: 'grey.300',
-                borderRadius: 2,
-              }}>
-              <Typography variant="h5" gutterBottom>Basic</Typography>
-              <Typography variant="h6" gutterBottom>$5 / Month</Typography>
-              <Typography>
-                { ' ' }
-                Access to basic flashcard features and limited storage.
-              </Typography>
-              <Button variant="contained" color="primary" sx={{mt: 2}}>
-                Choose Basic
-              </Button>
-            </Box>
+            <CardContainer className="inter-var">
+              <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+                <CardItem
+                  translateZ="50"
+                  className="text-xl font-bold text-neutral-600 dark:text-white"
+                >
+                  Basic
+                </CardItem>
+                <CardItem
+                  as="p"
+                  translateZ="60"
+                  className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                >
+                  $5 / Month
+                </CardItem>
+                <CardItem
+                  as="p"
+                  translateZ="60"
+                  className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                >
+                  Access to basic flashcard features and limited storage.
+                </CardItem>
+                <div className="flex justify-between items-center mt-20">
+                  <button className="p-[3px] relative" onClick={handleSubmit}>
+                    <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+                    <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
+                      Choose Basic
+                    </div>
+                  </button>
+                </div>
+              </CardBody>
+            </CardContainer>
           </Grid>
           <Grid item xs={12} md={6}>
-            <Box sx={{
-                p: 3,
-                border: '1px solid',
-                borderColor: 'grey.300',
-                borderRadius: 2,
-              }}>
-              <Typography variant="h5" gutterBottom>Pro</Typography>
-              <Typography variant="h6" gutterBottom>$10 / Month</Typography>
-              <Typography>
-                { ' ' }
-                Unlimited flashcards and storage, with priority support.
-              </Typography>
-              <Button variant="contained" color="primary" sx={{mt: 2}} onClick={handleSubmit}>
-                Choose Pro
-              </Button>
-            </Box>
+            <CardContainer className="inter-var">
+              <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+                <CardItem
+                  translateZ="50"
+                  className="text-xl font-bold text-neutral-600 dark:text-white"
+                >
+                  Pro
+                </CardItem>
+                <CardItem
+                  as="p"
+                  translateZ="60"
+                  className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                >
+                  $10 / Month
+                </CardItem>
+                <CardItem
+                  as="p"
+                  translateZ="60"
+                  className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                >
+                  Unlimited flashcards and storage, with priority support.
+                </CardItem>
+                <div className="flex justify-between items-center mt-20">
+                  <button className="p-[3px] relative" onClick={handleSubmit}>
+                    <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+                    <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
+                      Choose Pro
+                    </div>
+                  </button>
+                </div>
+              </CardBody>
+            </CardContainer>
           </Grid>
         </Grid>
       </Box>
